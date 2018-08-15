1824bda63bcd80421653f6396932d294447a6336
ret87 <umccomua@ya.ru>
2018-08-15 22:19:12 +0300

    js alerts confirm and prompt

---
+++ L1/main.js
@@ -0,0 +1,23 @@
+// alert
+
+// alert( "Привет" );
+// alert( "Привет1" );
+// alert( "Привет2" );
+
+
+// prompt
+
+// var years = prompt('Сколько вам лет?', 100);
+// alert('Вам ' + years + ' лет!');
+
+
+// confirm
+
+// var isAdmin = confirm ('вы админ?');
+// alert(isAdmin);
+
+
+// practice
+
+// var urname = prompt('как вас зовут?', '')
+// alert(urname);
