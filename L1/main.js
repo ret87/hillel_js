6525afc6ac7d5a0d46c17a69b28a5243076cd7cf
ret87 <umccomua@ya.ru>
2018-08-16 02:08:09 +0300

    js async and defer

--- L1/main.js
+++ L1/main.js
@@ -26,5 +26,15 @@
 // alert(urname);
 
 
+// async defer 
+// function addScript(src){
+//     var script = document.createElement('script');
+//     script.src = src;
+//     script.async = false; // чтобы гарантировать порядок
+//     document.head.appendChild(script);
+//   }  
+//   addScript('1.js'); // загружаться эти скрипты начнут сразу
+//   addScript('2.js'); // выполнятся, как только загрузятся
+//   addScript('3.js'); // но, гарантированно, в порядке 1 -> 2 -> 3
 
 
