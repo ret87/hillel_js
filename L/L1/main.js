c1d9ae70eec11c2d5613e9f0fa3759d168a33686
ret87 <umccomua@ya.ru>
2018-08-17 17:08:18 +0300

    add 1.2 hw

--- L/L1/main.js
+++ L/L1/main.js
@@ -56,32 +56,3 @@ console.log('lection 1');
 // alert(color);
 // вызов переменной при алерте - без кавычек - как бы обращаемся к ней - в кавычках будет конкретный текс.
 // name1 = 'art1'; при 'use strict' нельзя.
-
-
-// чётность
-var a = +prompt('введите целое число', 100);
-console.log(a);
-var b = +prompt('введите целое число', 100);
-console.log(b);
-var c = +prompt('введите целое число', 100);
-console.log(c);
-var sum = a + b + c;
-console.log(sum);
-alert(sum);
-
-if ( a & 1 ) {
-    // alert( "a нечётно" );
-} else {
-    alert( "a чётноe" );
-}
-if ( b & 1 ) {
-    // alert( "b нечётно" );
-} else {
-    alert( "b чётноe" );
-}
-if ( c & 1 ) {
-    // alert( "c нечётно" );
-} else {
-    alert( "c чётноe" );
-}
-
