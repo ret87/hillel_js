c91018efcd42462a73192f0d7f8642915f07e2d6
ret87 <umccomua@ya.ru>
2018-08-26 23:19:13 +0300

    have problems with label WTF???

--- L/L3/cycles/main.js
+++ L/L3/cycles/main.js
@@ -1,3 +1,20 @@
 'use strict';
 console.log('lection 1');
 
+// while 
+// var n = 0;
+// var x = 0;
+// while (n < 3) {
+//   n++;
+//   x += n;
+// console.log(x, n);
+// }
+
+outer:
+var x=+prompt('enter X', 3);
+while(x<=3){
+  x++;
+  console.log(x);
+  // if(x==1) break outer ?????????????????????
+}
+alert('Готово!');
