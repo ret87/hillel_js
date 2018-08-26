1d7c1b25ebc23d76ed54fd9b7500d3f34ac089eb
ret87 <umccomua@ya.ru>
2018-08-26 16:57:29 +0300

    add HW2 - 3 tasks

--- HW/HW2/main.js
+++ HW/HW2/main.js
@@ -1,4 +1,39 @@
 'use strict';
 console.log('lection 1');
 
+// 1. Вывести n чисел из ряда Фибоначчи, где n - вводится с клавиатуры
+
+// var n = +prompt('vvedite chislo fibonachi', 1);
+// var a = 1, b = 1;
+// for (var i = 3; i <= n; i++) {
+//     var c = a + b;
+//     a = b;
+//     b = c;
+//     console.log(c);
+// }
+
+
+
+// 2. Ввести с клавиатуры a и b, где а меньше b. Запустить цикл перебора от a до b с шагом h (ввод с клавиатуры). 
+// Посчитать сумму факториалов* чисел которые попадаются во время перебора.
+
+// var a = +prompt('vvedite nachalo', 1);
+// var b = +prompt('vvedite konec', 5);
+// var h = +prompt('vvedite shag ', 1)
+// var n;
+// for (var i=a; i < b; i=i+h) {
+//     console.log(i);
+//     while(n=!0 && n<i) {
+//         n=i*(i-h);
+//         console.log('factorial', n);
+//     }
+// }
+
+// 3. Выведите столбец четных чисел в промежутке от 0 до 100. (в document в виде таблицы)
+for (var i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+        document.write(i + '<br>');
+    }
+}
 
