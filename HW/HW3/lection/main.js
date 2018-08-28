7b40c5028babb40330e454cbe1dab8534a2216e7
ret87 <umccomua@ya.ru>
2018-08-28 17:40:03 +0300

    did 1st lection task

--- HW/HW3/lection/main.js
+++ HW/HW3/lection/main.js
@@ -1,3 +1,19 @@
 'use strict';
-console.log('Home Tasks №2.1, №2.2, №2.3');
+console.log('Lection №3 Tasks');
 
+
+// 1.
+// Дано a = 1, b = 15;. Вывести на экран таблицу, которая будет состоять из 2-ух рядков. 
+// В 1-ом рядке - нечетные числа: 13579111315, в 2-ом - четные: 2468101214
+
+// var a = 1, b = 15;
+// for (var i=a; i<=b;++i){
+//     if(i%2==0){
+//         console.log('chetnue = ', i);
+//         document.write('<td>' +'&nbsp' + '&nbsp' + i + '<br>' + '</td>');
+//     }
+//     if(i%2!==0){
+//         console.log('nechetnue = ', i);
+//         document.write('<td>' + i + ' </td>');
+//     }
+// }
