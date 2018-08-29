b6d08ababdf8433da03de38c9fd4ea898cc77641
ret87 <umccomua@ya.ru>
2018-08-29 20:35:25 +0300

    did 2nd and 3rd home tasks

--- HW/HW3/home/main.js
+++ HW/HW3/home/main.js
@@ -1,2 +1,36 @@
 'use strict';
 console.log('Home Tasks №3.1, №3.2, №3.3 №3.4');
+
+// №3.2 Вводится число. Преобразовать его в другое число, 
+// цифры которого будут следовать в обратном порядке по сравнению с введенным числом.
+
+// var a = +prompt('vvedite chislo', 5);
+// console.log('Vu vveli ' + a);
+// var b = a*Math.floor(Math.random() * 10);
+// console.log('Vawe chislo preobrazovalos` v ' + b);
+// var c;
+// while(a>=b){
+//     c= a--;
+//     console.log(c);
+// }if(c==b);
+// while(b>=a){
+//     c= b--;
+//     console.log(c);
+// }if(c==b);
+
+
+// №3.3 Дано целое число, состоящее из разных цифр. 
+// Определить, какая из цифр заданного числа больше, т. е. найти наибольшую цифру числа.
+
+// var n= +prompt('vvedite chislo iz 2h ne povtor cifr', 58);
+// console.log(n);
+// var c = Math.floor(n/10);
+// var b =n-c*10;
+// console.log(c);
+// console.log(b);
+// if(c>b){
+//     alert(c);
+// } else 
+// alert('naibol`waja cifra iz '+ n + ' eto ' + b);
+// console.log('naibol`waja cifra iz '+ n + ' eto ' + b);
+
