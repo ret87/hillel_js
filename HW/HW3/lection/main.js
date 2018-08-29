294f894a5a2cf0824fb78aad16142d983f6d3581
ret87 <umccomua@ya.ru>
2018-08-29 18:39:59 +0300

    did 5th lection task

--- HW/HW3/lection/main.js
+++ HW/HW3/lection/main.js
@@ -91,11 +91,9 @@ console.log('Lection №3 Tasks');
 // 4. Дано числа x, y, data, где data в диапазоне от x до y.
 // Вывести все числа от x до y, которые отличаются от data на 5 едениц по значению.
 
-
-// var x = 1;
-// var y = 50;
-// var i;
-// var data = 23;
+// var x = prompt('vvedite chislo X', 1);
+// var y = prompt('vvedite chislo Y, bol`we X', 50);
+// var data = prompt('vvedite chislo mejdy X i Y', 25);
 // while(x<data){
 //     data = data-5;
 //     if(data>x){
@@ -109,3 +107,85 @@ console.log('Lection №3 Tasks');
 //     }
 // }
 
+
+// 5. Нарисовать фигуру - прямоугольник. 
+// Фигура должна строить при помощи символов: *, &nbsp;. Высота и ширина задается переменными n, m с клавиатуры.
+//  *************
+//  *           *
+//  *           *
+//  *           *
+//  *************
+
+
+// 5.1
+// var n = 20;
+// var m = 15;
+// var i;
+// var j;
+// // document.write('*');
+// for(i = 1; i <= n; i++){
+//     document.write('*');
+// }
+// document.write('<br />');
+// for(i = 1; i <= m; i++){
+//     for(j = 1; j <= n; j++){
+//         if (j == 1 || j == n) {
+//             document.write('*');
+//         } else {
+//             document.write('&nbsp; ');
+//         }
+//     }
+//     document.write('<br />');
+// }
+// for(i = 1; i <= n; i++){
+//     document.write('*');
+// }
+// document.write('<br />');
+
+
+// 5.2
+// var n = 20;
+// var m = 5;
+// var i;
+// var sym = '*';
+// var separator = '&nbsp; ';
+// var fullLine = '';
+// var spaceLine = '';
+
+// for(i = 1; i <= n; i++){
+//     fullLine += sym;
+// } 
+// for(i = 1; i <= n; i++){
+//     if (i == 1 || i == n){
+//         spaceLine += sym;
+//     } else {
+//         spaceLine += separator;
+//     }
+// } 
+// // document.write(fullLine);
+// // document.write('<br/>');
+// // document.write(spaceLine);
+
+
+// document.write(fullLine + '<br/>');
+// for (i = 1; i <= m - 2; i++) {
+//     document.write(spaceLine + '<br/>');
+// }
+// document.write(fullLine + '<br/>');
+
+
+// 5.3
+// var w = prompt('vvedite wuriny', 10);
+// var h = prompt('vvedite vusoty', 20);
+// var x;
+// var y;
+// for(y = 1; y <= h; y++) {
+//     for(x = 1; x <= w; ++x) {
+//         if (y == 1 || y == h || x == 1 || x == w) {
+//             document.write('* &nbsp;');
+//         } else {
+//             document.write('&nbsp; &nbsp; ');
+//         }
+//     }
+//     document.write('<br />');
+// }
