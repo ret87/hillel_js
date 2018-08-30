1898ec13a3832881422751ebb8b98b3a6212e15e
ret87 <umccomua@ya.ru>
2018-08-30 20:56:06 +0300

    did 5th theory task

--- L/L4/Array/main.js
+++ L/L4/Array/main.js
@@ -275,14 +275,15 @@
     // При этом ноль 0 не должен заканчивать ввод, это разрешённое число.
     // Выводит сумму всех значений массива
 
-var numbers = [];
-while (true) {
-    var value = prompt("Введите число", 0);
-    if (value === "" || value === null || isNaN(value)) break;    
-    numbers.push(+value);
-}
-var sum = 0;
-for (var i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-}
-alert( sum );
+// var numbers = [];
+// while (true) {
+//     var value = prompt("Введите число", 0);
+//     if (value === "" || value === null || isNaN(value)) break;    
+//     numbers.push(+value);
+// }
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+// alert( sum );
+
