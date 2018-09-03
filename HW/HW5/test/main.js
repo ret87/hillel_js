2ba0fda5195c4527b12d5e35d8ae55c5cb27f19f
ret87 <umccomua@ya.ru>
2018-09-03 21:03:16 +0300

    did 2nd lvl test

--- HW/HW5/test/main.js
+++ HW/HW5/test/main.js
@@ -14,8 +14,8 @@ var str = Arr.join('_');
 alert( str ); 
 
 //1.2
-var A = [6,7,2,3,5,7];
-var Aul = A.join('</li><li>');
+var Arr2 = [6,7,2,3,5,7];
+var Aul = Arr2.join('</li><li>');
 document.write('<ul><li>');
 document.write(Aul);
 document.write('</li></ul>');
@@ -33,19 +33,31 @@ console.log(Arr3);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // level 2
 // 2.1 Организовать циклический ввод с клавиатуры элементов, которые будут добавляться к существующему массиву A = [1,2,3] в конец. 
 // Предусмотреть символ прекращения ввода «&»
 // 2.2 4 пункт, в начало
+
+// 2.1 
+var A1 = [1, 2, 3];
+console.log(A1);
+for(var i=0;i<A1.length;i++){
+    if(i<A1.length){
+        var A1Prompt = prompt('vvedite chislo', 5);
+        if (A1Prompt === '&' ) break;
+        A1.splice(A1.length+1, 0, A1Prompt); 
+        console.log(A1);
+    } 
+}
+
+//2.2 
+var A2 = [1, 2, 3];
+console.log(A2);
+for(var i=0;i<A2.length;i++){
+    if(i<A2.length){
+        var A2Prompt = prompt('vvedite chislo', 5);
+        if (A2Prompt === '&' ) break;
+        A2.splice(0, 0, A2Prompt);
+        console.log(A2);
+    } 
+}
