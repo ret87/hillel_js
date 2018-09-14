c1fa1fd41f34276693947d1c693bd32cba260071
ret87 <umccomua@ya.ru>
2018-09-14 21:51:27 +0300

    did 2nd task for HW6

--- HW/HW6/main.js
+++ HW/HW6/main.js
@@ -33,33 +33,24 @@ console.log(doubleArray(7,7));
 // Тот массив, сумма которого большая - должен вернутся функцией.
 var arr1 = [11,12,32,46,75,36,17,78,29]; 
 var arr2 = [13,29,13,49,75,36,57,82,98]; 
-// function arroys(arr1, arr2){
-//     for(var i=0;i<arr1.length;i++){
-//         var sumArr1 = 0;
-//         sumArr1 += arr1[i];
-//     }
-//     for(var i=0;i<arr2.length;i++){
-//         var sumArr2 = 0;
-//         sumArr2 += arr2[i];
-//     }
-//     if(sumArr1 < simArr2){
-//         return arr = sumArr2;
-//     }
-// }
-// arroys();
 function arrays(arr1, arr2){
     var sumArr1 = 0;
+    var sumArr2 = 0;
     for(var i=0;i<arr1.length;i++){
         sumArr1 += arr1[i];
     }
-    console.log(sumArr1);
-    var sumArr2 = 0;
     for(var i=0;i<arr2.length;i++){
         sumArr2 += arr2[i];
     }
-    console.log(sumArr2);
-    // if(sumArr1 < sumArr2){
-    //     return sumArr2;
-    // }
+    if(sumArr1 < sumArr2){
+        return bigArray(sumArr2);
+    } else {
+        return bigArray(sumArr1);
+    }
+    function bigArray(biggest){
+        console.log(biggest);
+    }
 }
 arrays(arr1, arr2);
+
+// 3. Задача
