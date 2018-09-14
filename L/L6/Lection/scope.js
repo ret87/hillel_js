7475229687f0a6fbcae8934e279eff85658bb7e0
ret87 <umccomua@ya.ru>
2018-09-14 17:02:05 +0300

    begin 2nd part for Lection (scope) at Lection 6

--- L/L6/Lection/scope.js
+++ L/L6/Lection/scope.js
@@ -1,2 +1,12 @@
 'use strict';
 console.log('Functions');
+
+// Области видимости
+// Желательно избегать создания переменных внутри самой функции (неявные переменные) - их стоит создать пустыми до функции
+var res;
+function sum (a,b){
+    res = a + b;
+    console.log(res);
+    return  res;
+}
+sum(1,2);
