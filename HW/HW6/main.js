f1de2ae7a73fb7fbfd20724eba73321b020b03a0
ret87 <umccomua@ya.ru>
2018-09-16 17:17:50 +0300

    did task №5 at HW6

--- HW/HW6/main.js
+++ HW/HW6/main.js
@@ -96,8 +96,6 @@ func("hello world", ['l', 'd']);
 //  function isEven(x) { return x % 2 == 0; } // проверяет на четность
 //  console.log(filter(input, isEven)); // [2, 4, 6]
 
-// function filter (input, isEven) {
-
 var arr = [];
 var input = [1,2,3,4,5,6];    
 function isEven(x){ 
