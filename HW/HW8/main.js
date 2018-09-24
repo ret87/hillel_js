00a9672c2ad2cf11ba687192ee5016bc42634b6a
ret87 <umccomua@ya.ru>
2018-09-24 19:53:29 +0300

    did 1st task at HW8

--- HW/HW8/main.js
+++ HW/HW8/main.js
@@ -5,6 +5,19 @@ console.log('Objects');
 // Сделайте функцию inArray('hello', ['svnj', 'hello', 'cvpoq']), которая определяет, есть в массиве элемент с заданным текстом или нет. 
 // Функция первым параметром должна принимать текст элемента, а вторым - массив, в котором делается поиск. 
 // Функция должна возвращать true или false.
+var arr = ['svnj', 'hello', 'cvpoq'];
+function inArray(text, array){
+    for(var key in array){
+        if(array[key] == text){
+            var isTrue = array[key];
+        } 
+    }
+    if(isTrue == text){
+        return console.log(true);
+    } else return console.log(false);
+}
+inArray('hello', arr);
+
 
 // 2. Задача
 // В задачу п.3 из лекции добавить 3ий аргумента flag, который является boolean. Если флаг true, 
