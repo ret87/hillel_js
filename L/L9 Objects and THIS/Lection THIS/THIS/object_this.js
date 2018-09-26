ce28f5f85e920ceaaea74d412e5067ecec96684c
ret87 <umccomua@ya.ru>
2018-09-26 18:40:53 +0300

    edit 1st lection practice task at L9 - objects and THIS

--- L/L9 Objects and THIS/Lection THIS/THIS/object_this.js
+++ L/L9 Objects and THIS/Lection THIS/THIS/object_this.js
@@ -12,13 +12,41 @@ console.log("Задача 1");
 var obj = { 
     x: 10, 
     y: 20, 
-    renderObject: function(){
-        for(var key in this){
-            if(key != "renderObject" && typeof this[key] != "function" ){
-                console.log(key + ': ' + this[key] + ';');
-                document.write(key + ': ' + this[key] + ';' + '</br>');
-            }
+    b: 10,
+    brk: function(){
+    },
+    objInn: {
+        a: 10,
+        b: 20
+    },
+    // var 1
+    // renderObject: function(){
+        //     for(var key in this){
+            //         if(key == 'renderObject'){
+                //             continue;
+                //         }   
+                //         console.log(key + ': ' + this[key] + ';');
+                //         document.write(key + ': ' + this[key] + ';' + '<br />');
+                //     }
+            // },
+    // var 2
+    renderObject: renderObject,
+    list: [1,2,3,4,5]
+}
+function renderObject (){
+    for(var key in this){
+        // if(this[key] === arguments.callee){
+        if(this[key] === renderObject){
+            continue;
         }
+        if(typeof this[key] === 'object'){
+            // this[key].renderObject = arguments.callee ;
+            this[key].renderObject = renderObject ;
+            this[key].renderObject();
+            continue;
+        }
+        console.log(key + ': ' + this[key] + ';');
+        document.write(key + ': ' + this[key] + ';' + '<br />');
     }
 }
 obj.renderObject();
@@ -47,26 +75,35 @@ console.log(obj1.m2().m1().m3());
 console.log(obj1.m2().m1().m3().m1().m1());
 
 
-console.log("Задача 3");
-// Задача 3
+console.log("Задача 3, 4");
+// Задача 3 
 // в обьекте data существует метод addRecord, который аргументами получает любой набор объектов. 
 // Метод addRecord добавляет все свойства переданных объектов в data.
+//Задача 4
+// В метод addRecord добавляется последний необязательный аргумент flag, который указывает приоритет присвоения свойств 
+// с одинаковым названием. Если true - берется свойство из первоначального объекта this, если false - берется свойство из arguments. 
+// По умолчанию flag = false;
+
 var data = {
-    addRecord: function(){
+    addRecord: function(flag){
         for(var i=0; i<arguments.length;i++){
             for(var key in arguments[i]){
-                console.log(key);
-                this[key] = arguments[i][key];
+                // console.log(arguments[i]);        
+                
+                if(flag = true){
+                //     for(var keyObj in this){
+                    console.log(flag);        
+                    this[key] = arguments[i][key];
+                }
+                // } else {
+                // }
             }
-        } 
+        }
     },
     p: 600,
     str: 'hello',
     y: -50
 }
-data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50});
-console.log( data.x, // 50
-    data.y, // 20
-    data.z, // 30
-    data.p, // 600
-    data.str,); // 'hello');
+// data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50}, true);
+data.addRecord({x: 10}, {y: 20}, {z: 30, x: 50}, false);
+console.log(data);
