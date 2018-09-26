daaf8a3c7c1b4112bcf39248e8b8b11d421cb414
ret87 <umccomua@ya.ru>
2018-09-26 15:28:01 +0300

    did 2nd lection practice task at L9 - objects and THIS

--- L/L9 Objects and THIS/Lection THIS/THIS/object_this.js
+++ L/L9 Objects and THIS/Lection THIS/THIS/object_this.js
@@ -6,6 +6,7 @@ console.log('Objects THIS');
 
 console.log("Лекционная практика");
 console.log("Задача 1");
+// Задача 1
 // Задан обьект с любым количеством свойств. Одно из свойств является функция renderObject(), которая описана в window. 
 // При вызове метода obj.renderObject() -> выводит в document всё содержимое объекта, кроме самого метода renderObject
 var obj = { 
@@ -21,3 +22,26 @@ var obj = {
     }
 }
 obj.renderObject();
+
+
+console.log("Задача 2");
+// Задача 2
+// Реализовать объект с методами m1(), m2(), m3(). Должна быть возможность выполнять подобный код:
+var obj1 = {
+    step: 0,
+    m1: function(){
+        this.step++;
+        return this;
+    },
+    m2: function(){
+        this.step--;
+        return this;
+    },
+    m3: function(){
+        this.step;
+        return this;
+    }
+} 
+console.log(obj1.m1().m2().m3());
+console.log(obj1.m2().m1().m3());
+console.log(obj1.m2().m1().m3().m1().m1());
