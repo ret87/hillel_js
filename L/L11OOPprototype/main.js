4be9953c36f7739b3ec3a52c96232f1958c747be
ret87 <umccomua@ya.ru>
2018-10-09 22:59:21 +0300

    add theory about methods for works with proto for G11OOPPrototype

--- L/L11OOPprototype/main.js
+++ L/L11OOPprototype/main.js
@@ -23,6 +23,9 @@ console.log('OOP Prototype');
     // Object.create(null)
 // Объект, создаваемый при помощи Object.create(null) не имеет прототипа, а значит в нём нет лишних свойств. 
 
+    // Методы для работы с proto
+// Object.getPrototypeOf(); - выводит значения объектов прототипа (наследуемые)
+// Object.setPrototypeOf(obj, proto); - можно записывать в прототип 
 
 // - Если один объект имеет специальную ссылку __proto__ на другой объект, то при чтении свойства из него, 
 // если свойство отсутствует в самом объекте, оно ищется в объекте __proto__.
@@ -71,3 +74,27 @@ console.log(data.hasOwnProperty('toString') ? data.toString : undefined);
 // тоже самое с помощью Object.create(null)
 var data = Object.create(null);
 console.log(data.toString); 
+var dataNext = {
+    redLine: function(){}
+}
+data.__proto__ = dataNext;
+console.log(data.redLine);
+
+    // Методы для работы с proto
+// Object.getPrototypeOf(); - выводит значения объектов прототипа (наследуемые)
+var object = {
+    first: 1,
+    second: 2,
+    third: 3,
+    four: 4
+}
+var object2 = {
+    five: 5,
+    six: 6
+}
+object.__proto__ = object2;
+console.log(Object.getPrototypeOf(object));
+// Object.setPrototypeOf(obj, proto);- можно записывать в прототип 
+
+// Object.create(proto, descriptors); - создание объекта с прототипом: 
+// Создаёт пустой объект с __proto__, равным первому аргументу
