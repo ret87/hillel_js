5e171da51c1a8133dba6aa9ced93af9b0d0bd45c
ret87 <umccomua@ya.ru>
2018-10-12 18:03:24 +0300

    add some theory about __proto__ and function constructor at Lection to L11OOP

--- L/L11OOP/NEW/main.js
+++ L/L11OOP/NEW/main.js
@@ -5,16 +5,15 @@ console.log('OOP NEW');
 // function Person(x,y) {x,y, run};
 // p = new Person();
 
+// New - это создание екзепляра класса
 // NEW - третье правило поведения THIS:
 // * new - всегда создаёт новый объект;
 // * new - вызывает функцию к которой его прировняли;
 // * new - меняет поведение THIS и заставляет объект смотреть на новый, созданный при помощи new;
 function Person(x,y){
-    x=x;
-    y=y;
-    // var run;
-    var run = function(x,y){return run = x + y;};
-    // console.log(x,y,run)
+    this.x=x;
+    this.y=y;
+    this.run = function(){};
 };
 var p = new Person(10, 15);
 console.log(p);
