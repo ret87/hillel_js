824d7b595dc5b6cc86f8b6a6672aa93c49200785
ret87 <umccomua@ya.ru>
2018-10-13 16:17:25 +0300

    add theory about return in NEW to NEW at L11OOP

--- L/L11OOP/NEW/main.js
+++ L/L11OOP/NEW/main.js
@@ -14,6 +14,7 @@ console.log('Конструктор');
     // Конструктор;
 // - Конструктор - собирает объект на основе класса (функции), в примере ниже объект Р собирается на основе параметров функции Person;
 // - Функции-конструктор используют для того, что бы создавать несколько однотипных объектов;
+// - Функции-конструктор называют с большой буквы - что бы можно было сразу их опредялять.
 // function Person(value1,value2) {this.key1=value1, this.key2=value2};
 // obj1 = new Person(value1,value2);
 // obj2 = new Person(value1,value2);
@@ -38,3 +39,26 @@ console.log(Olga);
 
 console.log('Правила обработки return');
     // Правила обработки return
+// Если есть явный вызов return в функции-конструкторе: 
+//   - при вызове return с объектом, будет возвращен он, а не this - то есть return объект = ПЕРЕБИВАЕТ this;
+//   - при вызове return с примитивным значением, оно будет отброшено; - то есть обращаясь на прямую к ключу
+function Info(name, work){
+    this.name = name;
+    this.work = work;
+    return {name: "Lilja"}; // перебьёт this 
+}
+var Oskar = new Info('Oskar', 'football player');
+console.log(Oskar);
+
+function Info2(){
+    this.name = 'Lilu';
+    return "Lilja"; // НЕ перебьёт this 
+}
+var obj1 = new Info2();     // создавай объект через New - this смотрит на объект перед = New и покажет непосредственно объект
+console.log(obj1);          // покажет объект полностью
+console.log(obj1.name);     // покажет значение ключа на прямую
+console.log(obj1['name']);  // покажет значение ключа на прямую
+
+
+console.log('Создание методов в конструкторе'); // 
+    // Создание методов в конструкторе
