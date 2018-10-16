f63ff1a1b8e0b071adecb4c410145c5914163d1e
ret87 <umccomua@ya.ru>
2018-10-16 04:05:34 +0300

    add some theory about recrusion to theory.js

--- L/L11OOP/LearnJS/inherit_classesJS/main.js
+++ L/L11OOP/LearnJS/inherit_classesJS/main.js
@@ -1,3 +1,10 @@
-// 'use strict';
+'use strict';
 console.log('OOP Inherit classes');
 
+function pow (x, n){
+    if(n != 1){
+        return x * pow(x, n-1);  
+    } else 
+    return x;
+};
+console.log(pow(5,3));
