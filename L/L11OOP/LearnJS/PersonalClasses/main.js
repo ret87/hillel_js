006fc300d7f8cb54560e6d15096b702387f0e6d5
ret87 <umccomua@ya.ru>
2018-10-17 23:59:46 +0300

    begin theory Personal CLasses in LearnJS at L11OOP

--- L/L11OOP/LearnJS/PersonalClasses/main.js
+++ L/L11OOP/LearnJS/PersonalClasses/main.js
@@ -2,7 +2,8 @@
 console.log('OOP Personal classes');
 
 
-
+        // Свои классы на прототипах
+// Обычный конструктор - как мы ранее уже объявляли через new по стандартным классам
 
 
 
@@ -13,4 +14,22 @@ console.log('OOP Personal classes');
 
 
 console.log('');
+// Обычный конструктор - как мы ранее уже объявляли через new по стандартным классам
 
+function Constr (name) {
+    this.speed = 0;
+    this.name = name;
+    this.run = function(speed){
+        this.speed += speed;
+        console.log(this.name + ' бежит со скоростью ' + this.speed);
+    };
+    this.stop = function(speed){
+        this.speed = 0;
+        console.log(this.name + ' стоит');
+    };
+}
+var cat = new Constr ('simba');
+console.log(cat['speed']);
+cat.run(3);
+cat.run(12);
+cat.stop(3);
