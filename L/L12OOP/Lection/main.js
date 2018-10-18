f3629bbe644a4ca9f332c9f7f4cc4ad6deac6052
ret87 <umccomua@ya.ru>
2018-10-18 21:44:33 +0300

    did ta some practice at task№2 а forÑ Lection for L12OOP

--- L/L12OOP/Lection/main.js
+++ L/L12OOP/Lection/main.js
@@ -3,9 +3,12 @@ console.log('OOP Prototype');
 
 
 
-console.log('Лекционная практика');
-console.log('Задача 1');
 
+
+
+console.log('Лекционная практика');
+
+console.log('Задача 1');
 // Задача 1.
 // Создать класс new SuperArray(n, m, { min: 10, max: 55 }), который создаст массив размерностью n на m и заполнит случайными числами 
 // в диапазоне options.min - options.max. Массив сохраняется в екземпляр класса SuperArray.
@@ -18,7 +21,19 @@ function SuperArray(n, m, min, max){
             array[i][j] = min + Math.floor(Math.random() * (max + 1 - min));
         }
     }
-    return console.log(array); 
-}
+    return array;
+};
+
 var array = new SuperArray(5, 4, 10, 55);
-array;
+console.log(array);
+
+
+console.log('Задача 2');
+// Задача 2.
+// Создать метод render(separator), в прототипе. Который выведет двумерный массив в документ. С разделителем separator, под массивом.
+var arr = new SuperArray(3, 6, 1, 30);
+SuperArray.prototype.render = function(){
+    // this.separator
+}
+console.log(arr);
+console.log(SuperArray.prototype);
