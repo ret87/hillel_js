aeb4aee156106fdb6352f5d6877f849776e829bd
ret87 <umccomua@ya.ru>
2018-10-18 20:52:51 +0300

    did taskâ№1 аÑat Lection for L12OOP

--- L/L12OOP/Lection/main.js
+++ L/L12OOP/Lection/main.js
@@ -1,3 +1,24 @@
 'use strict';
-console.log('OOP');
+console.log('OOP Prototype');
 
+
+
+console.log('Лекционная практика');
+console.log('Задача 1');
+
+// Задача 1.
+// Создать класс new SuperArray(n, m, { min: 10, max: 55 }), который создаст массив размерностью n на m и заполнит случайными числами 
+// в диапазоне options.min - options.max. Массив сохраняется в екземпляр класса SuperArray.
+
+function SuperArray(n, m, min, max){
+    var array = new Array(n);
+    for(var i=0; i<array.length; i++){
+        array[i] = new Array(m);
+        for(var j=0; j<array[i].length;j++){
+            array[i][j] = min + Math.floor(Math.random() * (max + 1 - min));
+        }
+    }
+    return console.log(array); 
+}
+var array = new SuperArray(5, 4, 10, 55);
+array;
