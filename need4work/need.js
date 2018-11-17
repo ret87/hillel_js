34b7e3694d706c42ebc477b1ecf22c26a073c219
ret87 <umccomua@ya.ru>
2018-11-17 22:26:20 +0200

    edite drag n drop method for need.js

--- need4work/need.js
+++ need4work/need.js
@@ -1576,53 +1576,35 @@ block2.addEventListener('click', function(event){
     console.log('third', this === event.target);
 });
 
-
-// 
-                                   
-
-
-
-
-
-
+// Мышь: Drag'n'Drop - захватить мышью элемент и перенести его
 var ball = document.getElementById('ball');
-
 ball.onmousedown = function(e) {
-
-  var coords = getCoords(ball);
-  var shiftX = e.pageX - coords.left;
-  var shiftY = e.pageY - coords.top;
-
-  ball.style.position = 'absolute';
-  document.body.appendChild(ball);
-  moveAt(e);
-
-  ball.style.zIndex = 1000; // над другими элементами
-
-  function moveAt(e) {
-    ball.style.left = e.pageX - shiftX + 'px';
-    ball.style.top = e.pageY - shiftY + 'px';
-  }
-
-  document.onmousemove = function(e) {
+    var coords = getCoords(ball);
+    var shiftX = e.pageX - coords.left;
+    var shiftY = e.pageY - coords.top;
+    ball.style.position = 'absolute';
+    document.body.appendChild(ball);
     moveAt(e);
-  };
-
-  ball.onmouseup = function() {
-    document.onmousemove = null;
-    ball.onmouseup = null;
-  };
-
-}
-
-ball.ondragstart = function() {
-  return false;
+    ball.style.zIndex = 1000; // над другими элементами
+    function moveAt(e) {
+        ball.style.left = e.pageX - shiftX + 'px';
+        ball.style.top = e.pageY - shiftY + 'px';
+    }
+    document.onmousemove = function(e) {
+        moveAt(e);
+    };
+    ball.onmouseup = function() {
+        document.onmousemove = null;
+        ball.onmouseup = null;
+    };
 };
-
+ball.ondragstart = function() {
+    return false;
+};
 function getCoords(elem) {   // кроме IE8-
-  var box = elem.getBoundingClientRect();
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset
-  };
-}
+    var box = elem.getBoundingClientRect();
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+};
