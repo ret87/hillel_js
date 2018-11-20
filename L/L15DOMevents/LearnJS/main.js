240c1a65b3b7186fbf8bda2de3eb9d9b70d19d7e
ret87 <umccomua@ya.ru>
2018-11-20 23:08:54 +0200

    add to file system L15

---
+++ L/L15DOMevents/LearnJS/main.js
@@ -0,0 +1,11 @@
+'use strict';
+window.onload = function(){
+
+    console.log('LearnJS DOM Events 15');
+    
+    console.log('Задача №1');
+    // Задача №1
+
+    
+
+};
---
+++ L/L15DOMevents/Lection/main.js
@@ -0,0 +1,11 @@
+'use strict';
+window.onload = function(){
+
+    console.log('Lection 15 DOM Events');
+    
+    console.log('Задача №1');
+    // Задача №1
+
+    
+
+};
