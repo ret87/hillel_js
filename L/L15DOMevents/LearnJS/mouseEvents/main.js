60408e14205ea19fd132561cdc38f83f070652aa
ret87 <umccomua@ya.ru>
2018-11-25 01:12:13 +0200

    add to file system mouseEvents to LearnJS for L15DOMevents

---
+++ L/L15DOMevents/LearnJS/mouseEvents/main.js
@@ -0,0 +1,14 @@
+'use strict';
+window.onload = function(){
+
+    console.log('Lection 15 DOM Events');
+    
+    console.log('Mouse Events');
+
+    // // Мышь: клики, кнопка, координаты
+// 
+
+
+    
+
+};
