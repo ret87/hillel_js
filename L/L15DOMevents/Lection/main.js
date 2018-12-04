60824d244811f4ff311bf0d99560063b9e3707c7
ret87 <umccomua@ya.ru>
2018-12-05 01:28:22 +0200

    add theory about events spread to Lection for L15DOMevents

--- L/L15DOMevents/Lection/main.js
+++ L/L15DOMevents/Lection/main.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 window.onload = function(){
 
     console.log('Lection 15 DOM Events');
@@ -20,26 +20,38 @@ document.querySelector('.block').addEventListener('click', function(event){
 document.querySelector('body').addEventListener('click', function(event){
     console.log('body', event);
     // console.log('body', event.path);
-}, true);
+}, false);
 
 // По сути любое событие выполняется дважды. Что бы выполнение работало только по обратному пути -всплытие, используем третий параметр,
 // в методе addEventListener(event, funct(){}, capturing) - у него может быть 2 вида true/false, включен или выключен захват.
 // По умолчанию там false, если поставим true, то первым выполнится - событие body, а потому уже block.
 // Но если прировнять к перменной то будет в порядке очереди, или же регламентировать её с помощью false.
+// Вложенные елементы прокликиваются насквозь от своих родителей, вызывая тем самым, события родителей связанные с кликом.
+// При этом очередность их вызова будет зависить от 3го параметра в addEventListener capturing. true от родителя к чаилду, false-наборот
+// Понимая что сначала идут capturing, а потом Bubbling, можно управлять и задавать очередь всплытия елементов, указывая true/false 
 
 
 var block1 = document.querySelector('.block1');
 var block2 = document.querySelector('.block2');
 var block3 = document.querySelector('.block3');
-var isTunnel = false;
+// var isTunnel = false;
+var isTunnel = true;
+
 block1.addEventListener('click', function(event){
-    event.target.style.backgroundColor = 'red';
+    // event.target.style.backgroundColor = 'red';
+    this.style.backgroundColor = 'red';
+    console.log('block1')
 }, isTunnel);
 block2.addEventListener('click', function(event){
-    event.target.style.backgroundColor = 'green';
-}, isTunnel);
+    // event.target.style.backgroundColor = 'green';
+    this.style.backgroundColor = 'green';
+    console.log('block2')
+// }, isTunnel);
+}, false);
 block3.addEventListener('click', function(event){
-    event.target.style.backgroundColor = 'yellow';
+    // event.target.style.backgroundColor = 'yellow';
+    this.style.backgroundColor = 'yellow';
+    console.log('block3')
 }, isTunnel);
 
 
